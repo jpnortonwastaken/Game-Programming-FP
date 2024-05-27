d6e943c0fd84b2fbc3a92af9bd0f386671095067
JP-Norton <72099534+JP-Norton@users.noreply.github.com>
2024-05-27 19:48:59 -0400

    Added simple enemies
    Added simple enemies that can move back and forth from two selected points

---
+++ Game-Programming-FP/Assets/Scripts/Enemy_Patrol.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 3f;
+    public float waitTime = 0.5f;
+
+    private Transform target;
+    private bool isWaiting = false;
+
+    void Start()
+    {
+        target = pointA;
+    }
+
+    void Update()
+    {
+        if (!isWaiting)
+        {
+            if (Vector3.Distance(transform.position, target.position) < 0.1f)
+            {
+                StartCoroutine(WaitAtPoint());
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    IEnumerator WaitAtPoint()
+    {
+        isWaiting = true;
+        yield return new WaitForSeconds(waitTime);
+        target = target == pointA ? pointB : pointA;
+        isWaiting = false;
+    }
+}

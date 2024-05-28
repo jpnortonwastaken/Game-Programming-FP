2f1e0e4b3679ad3ea0365dd09215a0460eb82692
twodab <twodan11764@gmail.com>
2024-05-28 16:40:58 -0400

    Create MouseLook.cs

---
+++ Game-Programming-FP/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform playerBody;
+    public float mouseSensitivity;
+    float yaw = 0;
+    float pitch = 0;
+    public float distance = 8;
+
+
+    void Start()
+    {
+        playerBody = transform.parent.transform;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float moveY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //yaw
+        yaw += moveX;
+        playerBody.Rotate(Vector3.up * moveX); //this should move player body
+
+        //pitch
+        pitch -= moveY;
+
+        //direction vector
+        Vector3 direction = new Vector3(0, 0, -distance);
+
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0);
+        transform.position = playerBody.position + rotation * direction;
+
+        transform.LookAt(playerBody.position);
+    }
+}

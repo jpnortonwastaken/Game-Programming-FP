263c4becd1cff41ef36be30a3c3dc8b884ba9d0d
twodab <twodan11764@gmail.com>
2024-06-03 18:40:46 -0400

    Polished attack and added hitboxes

--- Game-Programming-FP/Assets/Scripts/PlayerAttack.cs
+++ Game-Programming-FP/Assets/Scripts/PlayerAttack.cs
@@ -6,8 +6,9 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
 
-    public float knockback = 5;
+    public static float knockback = 20;
     public GameObject slashEffect;
+    float pitch = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,6 @@ public class PlayerAttack : MonoBehaviour
         if (Input.GetButtonDown("Fire1"))
         {
             HitEnemy();
-            KnockBack();
 
         }
 
@@ -29,27 +29,22 @@ public class PlayerAttack : MonoBehaviour
 
     void HitEnemy()
     {
-        GameObject attack = Instantiate(slashEffect, transform.position + transform.forward, transform.rotation)
+        GameObject attack = Instantiate(slashEffect, transform.position + Camera.main.transform.forward, transform.rotation)
                 as GameObject;
         attack.transform.SetParent(gameObject.transform);
 
+        pitch = Camera.main.transform.rotation.eulerAngles.y;
+        pitch = Mathf.Clamp(pitch, -90f, 90f);
+        attack.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+
     }
 
-    void KnockBack()
+    public static void KnockBack()
     {
+        GameObject gameObject = GameObject.FindGameObjectWithTag("Player");
         Rigidbody rb = gameObject.GetComponent<Rigidbody>();
-        if (EnemyBehavior.hitEnemy && !PlayerMovement.grounded)
-        {
-            knockback = 6;
-        }
-        else if (EnemyBehavior.hitEnemy)
-        {
-            knockback = 5;
-        }
-        else
-        {
-            knockback = 0;
-        }
         rb.AddForce(-1 * Camera.main.transform.forward * knockback, ForceMode.Impulse);
+
+        
     }
 }

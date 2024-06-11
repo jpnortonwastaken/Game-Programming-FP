e1739b402135a45c5eab2dd341ba9e08c22e7d54
twodab <twodan11764@gmail.com>
2024-06-10 23:15:53 -0400

    added bullet

---
+++ Game-Programming-FP/Assets/BulletLaunch.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletLaunch : MonoBehaviour
+{
+    public float speed;
+	public GameObject player;
+    [Tooltip("From 0% to 100%")]
+    public float accuracy;
+    public float fireRate;
+    public AudioClip shotSFX;
+    public AudioClip hitSFX;
+    public List<GameObject> trails;
+
+    private Vector3 offset;
+    private bool collided;
+    private Rigidbody rb;
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+        rb = GetComponent <Rigidbody> ();
+		
+		transform.LookAt(player.transform.position + new Vector3(0, 1, 0));
+
+		if (shotSFX != null && GetComponent<AudioSource>()) {
+			GetComponent<AudioSource> ().PlayOneShot (shotSFX);
+		}
+
+        Destroy(gameObject, 3);
+    }
+
+	void FixedUpdate () {	
+			rb.velocity = gameObject.transform.forward * speed;
+	}
+
+	void OnCollisionEnter (Collision co) {
+		if (co.gameObject.tag != "Bullet" && !collided) {
+			collided = true;
+			
+			if (shotSFX != null && GetComponent<AudioSource>()) {
+				GetComponent<AudioSource> ().PlayOneShot (hitSFX);
+			}
+
+			Destroy(gameObject);
+		}
+	}
+
+}
